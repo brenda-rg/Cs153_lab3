039fc3551e47da02e7dc53f34ad7e4c931af666b
Brenda Rojas <129914320+brenda-rg@users.noreply.github.com>
2024-05-31 10:23:43 +0000

    test current open code

--- shm.c
+++ shm.c
@@ -29,12 +29,29 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
-
+  int i;
+  acquire(&(shm_table.lock));
+  for (i = 0; i< 64; i++) {
+      if(shm_table.shm_pages[i].id != id)
+        continue;
+      mappages(myproc()->pgdir, PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      shm_table.shm_pages[i].refcnt++;
+      *pointer = PGROUNDUP(myproc()->sz);
+      myproc()->sz += PGSIZE;
+      release(&(shm_table.lock));
+    }
+  for (i = 0; i< 64; i++) {
+      if(shm_table.shm_pages[i].id != 0)
+        continue;
+      shm_table.shm_pages[i].id = id;
+      shm_table.shm_pages[i].frame = kalloc();
+      shm_table.shm_pages[i].refcnt = 1;
+      mappages(myproc()->pgdir, PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      *pointer = PGROUNDUP(myproc()->sz);
+      myproc()->sz += PGSIZE;
+      release(&(shm_table.lock));
+    }
 //you write this
-
-
-
-
 return 0; //added to remove compiler warning -- you should decide what to return
 }
 
