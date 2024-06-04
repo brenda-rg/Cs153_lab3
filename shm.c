9ebcab2a61f62f2c61b9feba59235ab32e3b2e01
Brenda Rojas <129914320+brenda-rg@users.noreply.github.com>
2024-06-04 16:43:14 +0000

    edit file2

--- shm.c
+++ shm.c
@@ -52,6 +52,7 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
   int i;
+  initlock(&(shm_table.lock), "SHM lock");
   acquire(&(shm_table.lock));
   for (i = 0; i< 64; i++) {
       if(shm_table.shm_pages[i].id != id)
@@ -59,7 +60,7 @@ int shm_open(int id, char **pointer) {
       mappages(myproc()->pgdir, (void*) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
       shm_table.shm_pages[i].refcnt++;
       *pointer = (char *) PGROUNDUP(myproc()->sz);
-      myproc()->sz += PGSIZE;
+      //myproc()->sz += PGSIZE;
       release(&(shm_table.lock));
       return 0;
     }
@@ -68,49 +69,40 @@ int shm_open(int id, char **pointer) {
         continue;
       shm_table.shm_pages[i].id = id;
       shm_table.shm_pages[i].frame = kalloc();
-      shm_table.shm_pages[i].refcnt = 1;
       memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+      shm_table.shm_pages[i].refcnt = 1;
       mappages(myproc()->pgdir, (void *) PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
       *pointer = (char*) PGROUNDUP(myproc()->sz);
-      myproc()->sz += PGSIZE;
+      //myproc()->sz += PGSIZE;
       release(&(shm_table.lock));
       return 0;
     }
-//you write this
 release(&(shm_table.lock));
 return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
 int shm_close(int id) {
-//you write this too!
   int i;
   pte_t *pte;
   int found = 0;
   initlock(&(shm_table.lock), "SHM lock");
   acquire(&(shm_table.lock));
   for (i = 0; i< 64; i++) {
-    found = 1;
     if(shm_table.shm_pages[i].id == id) {
-      shm_table.shm_pages[i].refcnt--;
-      if(shm_table.shm_pages[i].refcnt > 0 ) {
-        release(&(shm_table.lock));
-        return 0;
-      }
-      shm_table.shm_pages[i].frame = 0;
-      shm_table.shm_pages[i].id = 0;
-      //shm_table.shm_pages[i].refcnt = 0;
-      pte = walkpgdir(myproc()->pgdir, (const void*) PGROUNDUP(myproc()->sz), 1);
-      *pte = 0;
-      release(&(shm_table.lock));
-      return 0;
+    found = 1;
+    shm_table.shm_pages[i].refcnt--;
+    if(shm_table.shm_pages[i].refcnt > 0 ) {
+      break;
+    }
+    *(pte = walkpgdir(myproc()->pgdir, (void *) PGROUNDUP(myproc()->sz), 0)) = 0;
+    shm_table.shm_pages[i].frame = 0;
+    shm_table.shm_pages[i].id = 0;
     }
   }
-  if(found == 0) {
-    release(&(shm_table.lock));
+  release(&(shm_table.lock));
+  if(!found) {
     return 1; //no matched id found;
   }
-
-  release(&(shm_table.lock));
   return 0; //added to remove compiler warning -- you should decide what to return
 }
